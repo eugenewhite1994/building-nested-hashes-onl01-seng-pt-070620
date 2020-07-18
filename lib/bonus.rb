8f9899deaf8a84be1afbdbdcf6d2e6bec266ed9d
Eugene White <eugenewhite1994@gmail.com>
2020-07-18 21:34:26 +0000

    Automatically backed up by Learn

--- lib/bonus.rb
+++ lib/bonus.rb
@@ -9,7 +9,7 @@ def bonus
          {name: "Steven", age: "30", attitude: "confused"}
       ]
    }, 
-   },
+   
    :capulet => {
       :patriarch => {name: "Lord Capulet", age: "50"},
       :matriarch => {name: "Lady Capulet", age: "51"},
